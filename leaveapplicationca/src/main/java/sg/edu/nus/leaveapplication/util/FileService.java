1014224d535bb3cd796e93503b90389268901ab3
LaiSi <LaiSi@windows10.microdone.cn>
2019-05-24 18:30:47 +0800

    html changes and csv changes

--- leaveapplicationca/src/main/java/sg/edu/nus/leaveapplication/util/FileService.java
+++ leaveapplicationca/src/main/java/sg/edu/nus/leaveapplication/util/FileService.java
@@ -3,6 +3,7 @@ package sg.edu.nus.leaveapplication.util;
 import org.springframework.stereotype.Service;
 
 import sg.edu.nus.leaveapplication.model.Credentials;
+import sg.edu.nus.leaveapplication.model.Employee;
 import sg.edu.nus.leaveapplication.util.CsvUtils;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -13,21 +14,29 @@ import java.util.List;
 	public class FileService {
 
 
-	    public byte[] exportUsersToCsv(List<Credentials> cre) {
+	    public byte[] exportUsersToCsv(List<Employee> users) {
 	        // 为了方便，也不写dao层
-	        List<LinkedHashMap<String, Object>> exportData = new ArrayList<>(cre.size());
+	        List<LinkedHashMap<String, Object>> exportData = new ArrayList<>(users.size());
 	        // 行数据
-	        for (Credentials user : cre) {
+	        for (Employee user : users) {
 	            LinkedHashMap<String, Object> rowData = new LinkedHashMap<>();
-	            rowData.put("1", user.getUserId());
-	            rowData.put("2", user.getUsername());
-	            rowData.put("3", user.getPassword());
+	            rowData.put("1", user.getName());
+	            rowData.put("2", user.getDesignation());
+	            rowData.put("3", user.getLeaveEntitled());
+	            rowData.put("4", user.getContact());
+	            rowData.put("5", user.getEmail());
+	            rowData.put("6", user.getCompensationhours());
+	            rowData.put("7", user.getReportsTo());
 	            exportData.add(rowData);
 	        }
 	        LinkedHashMap<String, String> header = new LinkedHashMap<>();
-	        header.put("1", "用户账号");
-	        header.put("2", "用户昵称");
-	        header.put("3", "用户密码");
+	        header.put("1", "Name");
+	        header.put("2", "Designation");
+	        header.put("3", "LeaveEntitled");
+	        header.put("4", "Contact");
+	        header.put("5", "Email");
+	        header.put("6", "Compensationhours");
+	        header.put("7", "getReportsTo");
 	        return CsvUtils.exportCSV(header, exportData);
 	    }
 
